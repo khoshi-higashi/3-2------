e2ac6893bde10fbdb7742ceac1aa72e118135751
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-03-11 15:07:32 +0900

    ファイルを監視

--- highlight.py
+++ highlight.py
@@ -15,52 +15,56 @@ def crop_center(pil_img, crop_width, crop_height): # 画像の中心を切り出
                         (img_width + crop_width) // 2,
                         (img_height + crop_height) // 2))
 
-# ディレクトリが存在しない場合は作成する
-if not os.path.exists(dir_name):
-  os.mkdir(dir_name)
+def func():
+  # ディレクトリが存在しない場合は作成する
+  if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
 
-# ディレクトリが存在しない場合は作成する
-if not os.path.exists(new_dir_name):
-  os.mkdir(new_dir_name)
+  # ディレクトリが存在しない場合は作成する
+  if not os.path.exists(new_dir_name):
+    os.mkdir(new_dir_name)
 
-# ディレクトリが存在しない場合は作成する
-if not os.path.exists(used_dir_name):
-  os.mkdir(used_dir_name)
+  # ディレクトリが存在しない場合は作成する
+  if not os.path.exists(used_dir_name):
+    os.mkdir(used_dir_name)
 
-# glob.glob()で抽出された複数のファイルを一括で移動
-def move_glob(dst_path, pathname, recursive=True):
-  for p in glob.glob(pathname, recursive=recursive):
-    shutil.move(p, dst_path)
+  # glob.glob()で抽出された複数のファイルを一括で移動
+  def move_glob(dst_path, pathname, recursive=True):
+    for p in glob.glob(pathname, recursive=recursive):
+      shutil.move(p, dst_path)
 
-# pngファイルを移動
-move_glob(dir_name, '*.png')
-# jpgファイルを移動
-move_glob(dir_name, '*.jpg')
+  # pngファイルを移動
+  move_glob(dir_name, '*.png')
+  # jpgファイルを移動
+  move_glob(dir_name, '*.jpg')
 
-files = os.listdir(dir_name)
+  files = os.listdir(dir_name)
 
-i = 1
+  i = 1
 
-for file in files: # ホーム画面用の処理
-  im_original = Image.open(os.path.join(dir_name, file))
-  width, height = im_original.size
+  for file in files: # ホーム画面用の処理
+    im_original = Image.open(os.path.join(dir_name, file))
+    width, height = im_original.size
 
-  # ステータスバーを考慮して正方形にトリミング
-  im_crop = im_original.crop((0, 41+50, width, 41+50+width))
+    # ステータスバーを考慮して正方形にトリミング
+    im_crop = im_original.crop((0, 41+50, width, 41+50+width))
 
-  # 切り抜いた画像を保存
-  im_crop.save(os.path.join(new_dir_name, file))
+    # 切り抜いた画像を保存
+    im_crop.save(os.path.join(new_dir_name, file))
 
-  # 1枚ごとに完了を報告
-  print(str(i) + " done!")
-  i += 1
+    # 1枚ごとに完了を報告
+    print(str(i) + " done!")
+    i += 1
 
-# 使った画像は使用済みファイルに移動
-move_glob(used_dir_name, "./mikakou/*.PNG")
-move_glob(used_dir_name, "./mikakou/*.JPG")
+  # 使った画像は使用済みファイルに移動
+  move_glob(used_dir_name, "./mikakou/*.PNG")
+  move_glob(used_dir_name, "./mikakou/*.JPG")
 
-# 終了時に元の画像を削除
-shutil.rmtree(dir_name)
+  # 終了時に元の画像を削除
+  shutil.rmtree(dir_name)
 
-# すべて完了
-print("Complete!")
+  # すべて完了
+  print("Complete!")
+
+if __name__ == "__main__":
+  func()
