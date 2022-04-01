c9c2627faf907d24181dcae5acaf2b54ffd06c6d
khoshi-higashi <89444685+khoshi-higashi@users.noreply.github.com>
2022-04-01 10:23:18 +0900

    作成ファイル名変更

--- highlight.py
+++ highlight.py
@@ -1,106 +1,117 @@
 from PIL import Image, ImageChops
-import os # ファイルやフォルダ操作
+import os  # ファイルやフォルダ操作
 import glob
 import shutil
-import datetime # 現在時刻を取得
+import datetime  # 現在時刻を取得
 import pyocr
 
-#インストールしたTesseract-OCRのパスを環境変数「PATH」へ追記する。
-#OS自体に設定してあれば以下の2行は不要
-path='C:\\Program Files\\Tesseract-OCR'
+# インストールしたTesseract-OCRのパスを環境変数「PATH」へ追記する。
+# OS自体に設定してあれば以下の2行は不要
+path = 'C:\\Program Files\\Tesseract-OCR'
 os.environ['PATH'] = os.environ['PATH'] + path
 
-#pyocrへ利用するOCRエンジンをTesseractに指定する。
+# pyocrへ利用するOCRエンジンをTesseractに指定する。
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
 builder = pyocr.builders.TextBuilder(tesseract_layout=6)
 
-dir_name = "input" # 画像が入っているフォルダ
-new_dir_name = "output" # 画像を保存する先のフォルダ
-used_dir_name ="used"
+dir_name = "input"  # 画像が入っているフォルダ
+new_dir_name = "output"  # 画像を保存する先のフォルダ
+used_dir_name = "used"
 
-def crop_center(pil_img, crop_width, crop_height): # 画像の中心を切り出し
-  img_width, img_height = pil_img.size # 画像の横幅と縦の長さを取得
-  # 引数の長さにトリミング
-  return pil_img.crop(((img_width - crop_width) // 2,
-                        (img_height - crop_height) // 2,
-                        (img_width + crop_width) // 2,
-                        (img_height + crop_height) // 2))
+
+def crop_center(pil_img, crop_width, crop_height):  # 画像の中心を切り出し
+    img_width, img_height = pil_img.size  # 画像の横幅と縦の長さを取得
+    # 引数の長さにトリミング
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
 
 def ocr_name(im_original):
 
-  #画像から文字を読み込む
-  text = tool.image_to_string(im_original, lang="jpn", builder=builder)
+    # 画像から文字を読み込む
+    text = tool.image_to_string(im_original, lang="jpn", builder=builder)
 
-  list = []
-  list.append(text)
+    list = []
+    list.append(text)
 
-  # print(text.split("\n")[-2])
+    # print(text.split("\n")[-2])
 
-  return text.split("\n")[-2]
+    body = text.split("\n")[0]
+    body = body.replace("\"", "")
+    body = body.replace(" ", "")
+    title = text.split("\n")[-2]
+
+    return body, title
+
 
 def func():
-  # ディレクトリが存在しない場合は作成する
-  if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
+    # ディレクトリが存在しない場合は作成する
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
 
-  # ディレクトリが存在しない場合は作成する
-  if not os.path.exists(new_dir_name):
-    os.mkdir(new_dir_name)
+    # ディレクトリが存在しない場合は作成する
+    if not os.path.exists(new_dir_name):
+        os.mkdir(new_dir_name)
 
-  # ディレクトリが存在しない場合は作成する
-  if not os.path.exists(used_dir_name):
-    os.mkdir(used_dir_name)
+    # ディレクトリが存在しない場合は作成する
+    if not os.path.exists(used_dir_name):
+        os.mkdir(used_dir_name)
 
-  # glob.glob()で抽出された複数のファイルを一括で移動
-  def move_glob(dst_path, pathname, recursive=True):
-    for p in glob.glob(pathname, recursive=recursive):
-      shutil.move(p, dst_path)
+    # glob.glob()で抽出された複数のファイルを一括で移動
+    def move_glob(dst_path, pathname, recursive=True):
+        for p in glob.glob(pathname, recursive=recursive):
+            shutil.move(p, dst_path)
 
-  # pngファイルを移動
-  move_glob(dir_name, '*.png')
-  # jpgファイルを移動
-  move_glob(dir_name, '*.jpg')
+    # pngファイルを移動
+    move_glob(dir_name, '*.png')
+    # jpgファイルを移動
+    move_glob(dir_name, '*.jpg')
 
-  files = os.listdir(dir_name)
+    files = os.listdir(dir_name)
 
-  i = 1
+    i = 1
 
-  for file in files: # ホーム画面用の処理
+    for file in files:  # ホーム画面用の処理
 
-    im_original = Image.open(os.path.join(dir_name, file))
-    width, height = im_original.size
+        im_original = Image.open(os.path.join(dir_name, file))
+        width, height = im_original.size
 
+        # ステータスバーを考慮して正方形にトリミング
+        im_crop = im_original.crop((0, 41+50, width, 41+50+width))
 
-    # ステータスバーを考慮して正方形にトリミング
-    im_crop = im_original.crop((0, 41+50, width, 41+50+width))
+        dt_now = datetime.datetime.now()
+        # print(dt_now.strftime('%Y%m%d_%H%M%S'))
+        body, title = ocr_name(im_crop)
+        name = title
+        # name += "_"
+        # name += str(dt_now.strftime('%Y%m%d_%H%M%S'))
+        name += "_"
+        name += body
+        name += ".png"
 
-    dt_now = datetime.datetime.now()
-    # print(dt_now.strftime('%Y%m%d_%H%M%S'))
-    name = ocr_name(im_crop)
-    name += "_"
-    name += str(dt_now.strftime('%Y%m%d_%H%M%S'))
-    name += ".png"
+        # 切り抜いた画像を保存
+        # im_crop.save(os.path.join(new_dir_name, file))
+        im_crop.save(os.path.join(new_dir_name, name))
 
-    # 切り抜いた画像を保存
-    # im_crop.save(os.path.join(new_dir_name, file))
-    im_crop.save(os.path.join(new_dir_name, name))
+        # 1枚ごとに完了を報告
+        print(str(i) + " " + name + " done!")
+        i += 1
 
-    # 1枚ごとに完了を報告
-    print(str(i) + " " + name + " done!")
-    i += 1
+    # 使った画像は使用済みファイルに移動
+    move_glob(used_dir_name, "./input/*.PNG")
+    move_glob(used_dir_name, "./input/*.JPG")
 
-  # 使った画像は使用済みファイルに移動
-  move_glob(used_dir_name, "./input/*.PNG")
-  move_glob(used_dir_name, "./input/*.JPG")
+    # 終了時に元の画像を削除
+    # shutil.rmtree(dir_name)
 
-  # 終了時に元の画像を削除
-  # shutil.rmtree(dir_name)
+    # すべて完了
+    print("Exit a program")
 
-  # すべて完了
-  print("Exit a program")
 
 if __name__ == "__main__":
-  print("Execute a program")
-  func()
+    print("Execute a program")
+    func()
--- ocr.py
+++ ocr.py
@@ -2,12 +2,12 @@ import os
 from PIL import Image
 import pyocr
 
-#インストールしたTesseract-OCRのパスを環境変数「PATH」へ追記する。
-#OS自体に設定してあれば以下の2行は不要
-path='C:\\Program Files\\Tesseract-OCR'
+# インストールしたTesseract-OCRのパスを環境変数「PATH」へ追記する。
+# OS自体に設定してあれば以下の2行は不要
+path = 'C:\\Program Files\\Tesseract-OCR'
 os.environ['PATH'] = os.environ['PATH'] + path
 
-#pyocrへ利用するOCRエンジンをTesseractに指定する。
+# pyocrへ利用するOCRエンジンをTesseractに指定する。
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
@@ -19,16 +19,22 @@ files = os.listdir(input_dir)
 list = []
 
 for file in files:
-    #OCR対象の画像ファイルを読み込む
+    # OCR対象の画像ファイルを読み込む
     # img = Image.open("test.jpg")
     img = Image.open(os.path.join(input_dir, file))
 
-    #画像から文字を読み込む
+    # 画像から文字を読み込む
     builder = pyocr.builders.TextBuilder(tesseract_layout=6)
     text = tool.image_to_string(img, lang="jpn", builder=builder)
 
     list.append(text)
 
+    print(text)
+    print()
+    print(text.split("\n")[0])
+    print()
+    print(text.split("\n")[0].replace("\"", ""))
+    print()
     print(text.split("\n")[-2])
     print()
 # print(list)
